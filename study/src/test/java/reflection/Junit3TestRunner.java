6ecd44994e5562813023618561f12152cd04ecb0
sjin <sjin@konkuk.ac.kr>
2024-11-11 01:33:45 +0900

    finished step 0

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,25 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        // TODO Junit3Test에서 test로 시작하는 메소드 실행 -> 완료
+        final Constructor<Junit3Test> constructor = clazz.getConstructor();
+        final Junit3Test node = constructor.newInstance();
+
+        final Method[] methods = clazz.getMethods();
+        for(Method method: methods){
+            if(method.getName().startsWith("test")){
+                method.invoke(node);
+            }
+        }
+
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,24 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행 -> 완료
+        final Constructor<Junit4Test> constructor = clazz.getConstructor();
+        final Junit4Test node = constructor.newInstance();
+
+        final Method[] methods = clazz.getMethods();
+        for(Method method: methods){
+            if(method.isAnnotationPresent(MyTest.class)){
+                method.invoke(node);
+            }
+        }
     }
 }
